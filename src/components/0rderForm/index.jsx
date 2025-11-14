79a165369b6ecc6ae6298700178add760e573c93
Sebastian Rodriguez <sebasrm2000@gmail.com>
2025-11-13 22:41:37 -0500

    ref menu interface and delete orderForm

--- src/components/0rderForm/index.jsx
+++ src/components/0rderForm/index.jsx
@@ -21,31 +21,31 @@ const OrderForm = () => {
 
 
 //ENVIAR EL PEDIDO
-  const onSubmit =(data)=>{
-    console.log('orderFormDATA:',data)
-    //texto del link para google Maps
-let googleMapsUrl = `*Google Map:* https://www.google.com/maps/place/${data.direccionprincipal}%2B${data.direccionuno}%2B%2523%2B${data.direcciondos}%2B-%2B${data.direcciontres},%2BCorinto%2BCauca`
-if(selectedOption==='Consumo Local'){
-googleMapsUrl='';
-}
-    //texto con el nombre y direccion y nota
-    // const customerNameandAdress =`________________ %0A*Entregar a*: ${data.nombre},%0A*Tipo de Pedido:* ${selectedOption},%0A*Dirección:* ${data.direccionprincipal} ${data.direccionuno} %23${data.direcciondos}-${data.direcciontres}, ${data.barrio} %0A${data.notas?`*Notas:* ${data.notas}`:''}`;
+//   const onSubmit =(data)=>{
+//     console.log('orderFormDATA:',data)
+//     //texto del link para google Maps
+// let googleMapsUrl = `*Google Map:* https://www.google.com/maps/place/${data.direccionprincipal}%2B${data.direccionuno}%2B%2523%2B${data.direcciondos}%2B-%2B${data.direcciontres},%2BCorinto%2BCauca`
+// if(selectedOption==='Consumo Local'){
+// googleMapsUrl='';
+// }
+//     //texto con el nombre y direccion y nota
+//     // const customerNameandAdress =`________________ %0A*Entregar a*: ${data.nombre},%0A*Tipo de Pedido:* ${selectedOption},%0A*Dirección:* ${data.direccionprincipal} ${data.direccionuno} %23${data.direcciondos}-${data.direcciontres}, ${data.barrio} %0A${data.notas?`*Notas:* ${data.notas}`:''}`;
 
-    let customerNameandAdress = ``;
+//     let customerNameandAdress = ``;
 
-    if (selectedOption === 'Domicilio') {
-      customerNameandAdress = `________________ %0A*Entregar a*: ${data.nombre},%0A*Tipo de Pedido:* ${selectedOption},%0A*Dirección:* ${data.direccionprincipal} ${data.direccionuno} %23${data.direcciondos}-${data.direcciontres}, ${data.barrio} %0A${data.notas?`*Notas:* ${data.notas}`:''}`;
-    } else {
-      customerNameandAdress = `________________ %0A*Entregar a*: ${data.nombre},%0A*Tipo de Pedido:* ${selectedOption},%0A ${data.notas?`*Notas:* ${data.notas}`:''}`;
-    }
+//     if (selectedOption === 'Domicilio') {
+//       customerNameandAdress = `________________ %0A*Entregar a*: ${data.nombre},%0A*Tipo de Pedido:* ${selectedOption},%0A*Dirección:* ${data.direccionprincipal} ${data.direccionuno} %23${data.direcciondos}-${data.direcciontres}, ${data.barrio} %0A${data.notas?`*Notas:* ${data.notas}`:''}`;
+//     } else {
+//       customerNameandAdress = `________________ %0A*Entregar a*: ${data.nombre},%0A*Tipo de Pedido:* ${selectedOption},%0A ${data.notas?`*Notas:* ${data.notas}`:''}`;
+//     }
     
 
-    //texto link a whatsapp
-    const whatsappUrl = `https://api.whatsapp.com/send?phone=573234754284&text=${googleMapsUrl} %0A*¡Nuevo Pedido!*🛵%0A Restaurante Corralazo %0A*Productos*: %0A ${productsText} %0A*Valor total:* $${sum} %0A ${customerNameandAdress} `;
+//     //texto link a whatsapp
+//     const whatsappUrl = `https://api.whatsapp.com/send?phone=573234754284&text=${googleMapsUrl} %0A*¡Nuevo Pedido!*🛵%0A Restaurante Corralazo %0A*Productos*: %0A ${productsText} %0A*Valor total:* $${sum} %0A ${customerNameandAdress} `;
 
-    window.location.href = whatsappUrl; // Redirigir a WhatsApp
+//     window.location.href = whatsappUrl; // Redirigir a WhatsApp
 
-  }
+//   }
 
   const [selectedOption, setSelectedOption] = useState('Domicilio');
 
@@ -79,144 +79,25 @@ googleMapsUrl='';
   return (
     <div className='bg-[#000]/60 rounded-md w-[full] text-left mb-3'>
       <div className='flex flex-col  gap-1 mb-[10px]'>
-          <label htmlFor="">Tipo de Pedido</label>
+          <label htmlFor="">Mesa:</label>
          <select onChange={handleSelectChange} value={selectedOption} id=""
              >
-          <option value="Domicilio">Domicilio</option>
-          <option value="Consumo Local">Consumo Local</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
          </select>
       </div>
 
-{selectedOption === "Domicilio"?
- <form 
- onSubmit={handleSubmit(onSubmit)}
- className='border border-orange rounded-lg  p-[10px]' action="">
-   <div className=' flex flex-col  gap-1 mb-[10px]'>
-     <label htmlFor="">Nombre</label>
-     <input className='w-full'
-      type="text" 
-      {...register('nombre', {required:true,maxLength:10})}
-      value={formData.nombre}
-      onChange={handleInputChange}
-       />
-       {errors.nombre?.type==='required' && <p>El nombre es requerido</p>}
-       {errors.nombre?.type==='maxLength' && <p>El nombre debe tener menos de 10 caracteres</p>}
-   </div>
- 
-   <div className='flex flex-col  mb-[10px]' >
-     <label htmlFor="">Dirección</label>
-     <div className='flex gap-2'>
-       <select className='w-14'  
-       {...register('direccionprincipal', {
-         required:true
-        })} id=""
-        value={formData.direccionprincipal}
-        onChange={handleInputChange}
-        >
-         <option value="Cra.">carrera</option>
-         <option value="Cl.">calle</option>
-         <option value="diagonal">diagonal</option>
-       </select>
-       <input 
-       className='w-14'
-         {...register('direccionuno', {
-           required:true,
-           maxLength:3,
-          })}
-         type="text"
-         value={formData.direccionuno}
-  onChange={handleInputChange} />
-       <p className='text-white'>#</p>
-       <input 
-         className='w-14'
-         {...register('direcciondos', {
-           maxLength:3,
-           required:true
-          })}
-         type="text" 
-         value={formData.direcciondos}
-  onChange={handleInputChange}
-         />
-       <p className='text-white'>-</p>
-       <input 
-       className='w-14' 
-       {...register('direcciontres', {
-         maxLength:3,
-         required:true
-        })}
-        type="text" 
-        value={formData.direcciontres}
-  onChange={handleInputChange}
-        />
-
-
-     </div>
- 
-   </div>
-   <div className='flex flex-col mb-2'  >
-     <label htmlFor="">Barrio, Casa o punto de referencia?</label>
-     <input 
-     className='w-full' 
-     {...register('barrio', {
-       maxLength:50
-      })}
-     type="text"
-     value={formData.barrio}
-  onChange={handleInputChange}
-     />
-   </div>
-   <div className='flex flex-col mb-2 '  >
-     <label htmlFor="">Agrega notas a tu pedido y da click en "Enviar a WhatsApp"</label>
-     <input 
-     className='w-full' 
-     {...register('notas')}
-     type="text"
-     value={formData.notas}
-  onChange={handleInputChange}
-      />
-   </div>
-   <ShoppingCart sum={sum}/>
- </form>
- :
- <form
- onSubmit={handleSubmit(onSubmit)}
- className='border border-orange rounded-lg  p-[10px]' 
- >
-   <div className=' flex flex-col  gap-1 mb-[10px]'>
-     <label htmlFor="">Nombre</label>
-     <input className='w-full'
-      type="text" 
-      {...register('nombre', {
-       required:true,
-       maxLength:10
-      })}
-       />
-       {errors.nombre?.type==='required' && <p>El nombre es requerido</p>}
-       {errors.nombre?.type==='maxLength' && <p>El nombre debe tener menos de 10 caracteres</p>}
-   </div>
-
-   <div className=' flex flex-col  gap-1 mb-[10px]'>
-     <label htmlFor="">Mesa:</label>
-     <input className='w-full'
-      type="text" 
-      {...register('mesa', {
-       required:true,
-       maxLength:10
-      })}
-       />
-       {errors.mesa?.type==='required' && <p>El numero de mesa es requerido</p>}
-       {errors.mesa?.type==='maxLength' && <p>El numero es incorrecto</p>}
-   </div>
-   <div className='flex flex-col mb-2 '  >
-     <label htmlFor="">Agrega notas a tu pedido y da click en "Enviar a WhatsApp"</label>
-     <input 
-     className='w-full' 
-     {...register('notas')}
-     type="text" />
-   </div>
-   <ShoppingCart sum={sum}/>
-
-  </form>}
+      <ShoppingCart sum={sum}/>
      
     </div>
   )
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,22 +9,22 @@ const Header = () => {
     <header className='w-full text-white'>
         <div className='flex-col justify-between '>
             <div className='flex items-end gap-1'>
-            <figure>
+            {/* <figure>
                 <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/39ff23185580241.65887d5f1f855.png" alt="logo" />
-            </figure>
+            </figure> */}
             <h1 className='titlePage text-[2.8rem] text-orange'>Corralazo</h1>
             <BusinessStatus/>
             </div>
             <div className='w-[100%] mt-[10px] pl-[5px]'>
                 {/* <p className='subtitle'>Comidas Rapidas</p> */}
-                <div className=' gap-1 flex justify-evenly items-end'>
+                {/* <div className=' gap-1 flex justify-evenly items-end'>
                     <p className='text-[0.8rem] bg-orange rounded-sm px-1 text-white  '>Calle 5 #9-40 Corinto</p>
                     <p  className='text-[0.8rem]  bg-orange rounded-sm px-1 text-white '>Cel: 323 475 4284</p>
-                </div>
-                 <ol className='text-[0.7rem] sm:flex-col flex md:justify-evenly  bg-white rounded-sm text-dark-gray mt-1'>
+                </div> */}
+                 {/* <ol className='text-[0.7rem] sm:flex-col flex md:justify-evenly  bg-white rounded-sm text-dark-gray mt-1'>
                     <li> * Elige categoría y escoge tu producto</li>
                     <li> * Envia tu pedido y espera tu domicilio</li>
-                </ol>
+                </ol> */}
             </div>
         </div>
         <input className='w-[85%] h-[30px] mt-2 py-1 pr-1 text-[1.4rem]' 
